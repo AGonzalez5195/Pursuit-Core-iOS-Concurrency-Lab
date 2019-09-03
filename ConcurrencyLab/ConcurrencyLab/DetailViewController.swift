828315802011c99c57e7c6be7f7da82530236749
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-09-03 12:31:22 -0400

    Set up storyboard shit

---
+++ ConcurrencyLab/ConcurrencyLab/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ConcurrencyLab
+//
+//  Created by Anthony Gonzalez on 9/3/19.
+//  Copyright © 2019 Anthony. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
